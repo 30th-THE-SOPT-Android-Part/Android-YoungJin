21be773d5019431c9585c607f5c1fdd1df3bce5e
choiyoungjin <cyjin6789@gmail.com>
2022-05-02 00:20:11 +0900

    [feat] 갤러리 이미지 띄우기 구현

--- app/src/main/java/org/sopt/soptseminar/presentation/gallery/GalleryFragment.kt
+++ app/src/main/java/org/sopt/soptseminar/presentation/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package org.sopt.soptseminar.presentation.gallery
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.soptseminar.R
 import org.sopt.soptseminar.base.BaseFragment
@@ -11,7 +14,26 @@ import org.sopt.soptseminar.databinding.FragmentGalleryBinding
 class GalleryFragment : BaseFragment<FragmentGalleryBinding>(R.layout.fragment_gallery) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        addListeners()
     }
+
+    private fun addListeners() {
+        binding.imageContainer.setOnClickListener {
+            storagePermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private val storagePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                galleryLauncher.launch("image/*")
+            }
+        }
+
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) {
+            Glide.with(binding.galleryImage).load(it).into(binding.galleryImage)
+        }
 
     companion object {
         private const val TAG = "GalleryFragment"
