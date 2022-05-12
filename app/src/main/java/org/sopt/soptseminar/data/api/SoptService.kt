2f44b2041998ef8264966486faa66a2f81acda94
youngjinc <cyjin6789@gmail.com>
2022-05-13 04:18:57 +0900

    [feat] BaseResponse 데이터 클래스 생성 및 적용

--- app/src/main/java/org/sopt/soptseminar/data/api/SoptService.kt
+++ app/src/main/java/org/sopt/soptseminar/data/api/SoptService.kt
@@ -1,17 +1,14 @@
 package org.sopt.soptseminar.data.api
 
-import org.sopt.soptseminar.data.models.RequestSignIn
-import org.sopt.soptseminar.data.models.RequestSignUp
-import org.sopt.soptseminar.data.models.ResponseSignIn
-import org.sopt.soptseminar.data.models.ResponseSignUp
+import org.sopt.soptseminar.data.models.*
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface SoptService {
     @POST("auth/signin")
-    suspend fun postSignIn(@Body body: RequestSignIn): Response<ResponseSignIn>
+    suspend fun postSignIn(@Body body: RequestSignIn): Response<BaseResponse<ResponseSignIn>>
 
     @POST("auth/signup")
-    suspend fun postSignUp(@Body body: RequestSignUp): Response<ResponseSignUp>
+    suspend fun postSignUp(@Body body: RequestSignUp): Response<BaseResponse<ResponseSignUp>>
 }
--- app/src/main/java/org/sopt/soptseminar/data/models/ResponseSignIn.kt
+++ app/src/main/java/org/sopt/soptseminar/data/models/ResponseSignIn.kt
@@ -1,12 +1,6 @@
 package org.sopt.soptseminar.data.models
 
 data class ResponseSignIn(
-    val status: Int,
-    val message: String,
-    val data: Data,
-) {
-    data class Data(
-        val name: String,
-        val email: String
-    )
-}
+    val name: String,
+    val email: String
+)
